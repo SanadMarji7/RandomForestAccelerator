88a5bcafa3ef7b672ff2d0eb65614e73ae4440f0
SanadMarji7 <74622693+SanadMarji7@users.noreply.github.com>
2022-03-17 22:10:33 +0100

    Update Comparator.vhdl

--- vhdl/Comparator.vhdl
+++ vhdl/Comparator.vhdl
@@ -1,6 +1,9 @@
+--The Comparator is used to compare 2 4-bit values (features and thresholds)
+-- and decides whether the feature is greater or less_equal than the threshold 
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+--Entity has 2 inputs of 4 bits and 2 outputs (greater, less_equal) of 1 bit
 entity Comparator is
 	port(
 		feature : in std_logic_vector(3 downto 0);
@@ -10,6 +13,8 @@ entity Comparator is
 		);
 end Comparator;
 
+--Compares every bit from both inputs (feature and threshold) 
+--and gives 1 or 0 to Gout(greater), Eout(Equals) and Lout(Less)
 architecture behavior of Comparator is
 	procedure comp1bit(
 					ft: in std_logic;
@@ -21,11 +26,13 @@ architecture behavior of Comparator is
 					Eout: out std_logic) is
 	begin
 		Gout:= (ft and not th) or (ft and Gin) or (not th and Gin);
-		Eout:= (not ft and not ft and not Gin and not Lin) or
+		Eout:= (not ft and not th and not Gin and not Lin) or
 		       (ft and th and not Gin and not Lin);
 		Lout := (not ft and th) or (not ft and Lin) or (th and Lin);
 	end procedure;
-	
+
+--Comparison is done through a loop for each bit individually and the result is sent to the next loop. 
+--On the last iteration the result will be stored in the Outputs of the Comparator-Component.
 begin
 	process(feature,threshold)
 	variable G, L, E: std_logic_vector(4 downto 0);
@@ -39,4 +46,3 @@ begin
 		less_equal <= E(4) or L(4);
 	end process;
 end behavior;
-		
