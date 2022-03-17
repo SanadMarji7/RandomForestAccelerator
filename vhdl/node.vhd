22069f02fd20c94ae15d1d29b595f69cbd614a82
SanadMarji7 <74622693+SanadMarji7@users.noreply.github.com>
2022-03-17 20:17:53 +0100

    Update node.vhd

--- vhdl/node.vhd
+++ vhdl/node.vhd
@@ -1,7 +1,13 @@
+--node calculates the next node, and returns the address of the left/right child of current node.
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
+--node has:
+--3 input --> all_info(20 bit data of all node information containing threshold, feature, lc, rc, and class
+--            feature_to_compare: given from the DT design and is used by the comparator.
+--            current_node
+--1 output --> next_node: returns next_node to continue iteration through decision tree
 entity node is
     port(
         all_info : in std_logic_vector(19 downto 0);
@@ -13,6 +19,7 @@ end node;
 
 architecture Behavioral of node is
 
+    --component comparator used in design
     component comparator is
         port(
 		  feature : in std_logic_vector(3 downto 0);
@@ -26,14 +33,16 @@ architecture Behavioral of node is
     signal lc, rc, th, ft : std_logic_vector(3 downto 0);
     
 begin
+           --this is summary of lc,rc,th,ft address within all_info. each address corrosponds to a value
            lc <= all_info(11 downto 8);
            rc <= all_info(7 downto 4);
            th <= all_info(19 downto 16);
            ft <= all_info(15 downto 12);  
            
-     
+     --the comparator is given the feature, threshold and determines whether its greater than or lessthan/equal.
      comparator_instance : comparator port map(feature => feature_to_compare, threshold => th , greater => greater_than, less_equal => less_than_equal);
      
+     --in summary if feature was greater than threshold next_node will be the right child, and if not next_node = left child
      process(less_than_equal, greater_than)
      begin
      if(ft /= "1111") then
